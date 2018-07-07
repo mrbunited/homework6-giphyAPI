d5045fa2e23c98e6afc58b565e07a1a2d145b013
mradke2 <mrbunited@gmail.com>
2018-07-06 21:51:30 -0500

    js finished

---
+++ assets/javascript/game.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+   // Initial array
+      var animalsArray = ["cat", "dog", "bird"];
+      
+    function displayAnimalGifs(){
+       $("#images-display").empty();
+       var animalClick = $(this).attr("data-name");
+       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+       animalClick + "&api_key=dc6zaTOxFJmzC&limit=10&rating=g";
+ 
+       $.ajax({
+         url: queryURL,
+         method: "GET"
+       })
+   
+         .then(function(response) {
+           var imageUrl = [];
+          console.log(response);
+        for (var a = 0; a < response.data.length; a++) {
+        	
+        	var newDiv = $("<div>");
+
+        	var rating = response.data[a].rating;
+        	var animatedImg = response.data[a].images.fixed_height.url;
+        	var stillImage = response.data[a].images.fixed_height_still.url;
+        	var animalImage = $("<img>");
+        	var p = $("<p>").text("Rating: " + rating);
+
+        	animalImage.attr("src", stillImage);
+        	animalImage.addClass("gif");
+        	animalImage.attr("data-state", "still");
+        	animalImage.attr("data-still", stillImage);
+        	animalImage.attr("data-animate", animatedImg);
+        	newDiv.append(p);
+        	newDiv.append(animalImage);
+          $("#images-display").prepend(newDiv);
+}
+         });
+}
+function pauseGifs(){
+// $(".gif").on("click", function() {
+  var state = $(this).attr('data-state');
+  if (state === 'still') {
+    $(this).attr('src', $(this).attr('data-animate'));
+    $(this).attr('data-state', 'animate');
+  } else {
+    $(this).attr('src', $(this).attr('data-still'));
+    $(this).attr('data-state', 'still');
+  }
+// });
+}
+function renderButtons() {
+        $("#animal-view").empty();
+        for (var i = 0; i < animalsArray.length; i++) {
+        var addedButton = $("<button>");
+          addedButton.addClass("animal-btn btn btn-primary");
+           addedButton.attr("data-name", animalsArray[i]);
+         addedButton.text(animalsArray[i]);
+         $("#animal-view").append(addedButton);
+        }
+}
+
+$("#add-animal").on("click", function(event) {
+        event.preventDefault();
+        var animal = $("#animal-input").val().trim();
+        animalsArray.push(animal);
+        renderButtons();
+      });
+
+$(document).on("click", ".gif", pauseGifs);
+$(document).on("click", ".animal-btn", displayAnimalGifs);
+renderButtons();
+});
